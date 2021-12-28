b947607ea3a28e48ed7dfd2261c04b2627cd511d
Priyansh <youname972@gmail.com>
2021-12-28 01:38:00 -0500

    Done

--- Test.py
+++ Test.py
@@ -3,19 +3,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
 
-# # Use the `install()` method to set `executabe_path` in a new `Service` instance:
 service = Service(executable_path=ChromeDriverManager().install())
 
-# # Pass in the `Service` instance with the `service` keyword: 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+browser = webdriver.Chrome(chrome_options=options, service=service)
 
-browser = webdriver.Chrome(service=service)
 browser.maximize_window()
 browser.get("https://www.youtube.com/")
 
@@ -27,5 +25,8 @@ search = browser.find_element(By.CLASS_NAME, "ytd-searchbox")
 search.click()
 
 write = browser.find_element(By.NAME, "search_query")
-write.send_keys("ThinkOrSwim")
+write.send_keys("AJR")
 browser.find_element(By.ID, "search-icon-legacy").click()
+
+time.sleep(4)
+browser.close()
