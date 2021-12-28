2a63b7b077a928fdcf2fef78ca29be89e281a71c
Priyansh <youname972@gmail.com>
2021-12-28 11:45:15 -0500

    Lol

--- Test.py
+++ Test.py
@@ -1,37 +1,21 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-import time
-
-def youtube(website):
-
-    service = Service(executable_path=ChromeDriverManager().install())
-
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
-    browser = webdriver.Chrome(chrome_options=options, service=service)
-
-    browser.maximize_window()
-    browser.get("https://www.youtube.com/")
+import definition as d
+import PySimpleGUI as sg
 
 
-    #wait = WebDriverWait(browser, 3)
-    #visible = EC.visibility_of_element_located
+# website = input("What would you like to search on Youtube? ")
+# d.youtube(website=website)
 
-    search = browser.find_element(By.CLASS_NAME, "ytd-searchbox")
-    search.click()
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
 
-    write = browser.find_element(By.NAME, "search_query")
-    write.send_keys(website)
-    browser.find_element(By.ID, "search-icon-legacy").click()
+# Create the window
+window = sg.Window("Demo", layout, margins=(300,200))
 
-    time.sleep(10)
-    browser.close()
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
 
-website = input("What would you like to search on Youtube? ")
-youtube(website=website)
+window.close()
